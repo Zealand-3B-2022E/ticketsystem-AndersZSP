2429fd3af4c8622f33830ad0ad77e44c8c4a6f46
Anders Zune Sander Petersen <ande939h@edu.easj.dk>
2022-09-30 10:06:19 +0200

    Car class has been made and it's methods have been tested (Task 2)

---
+++ TicketSystemClassLibrary/Car.cs
@@ -0,0 +1,47 @@
+﻿namespace TicketSystemClassLibrary
+{
+    /// <summary>
+    /// Objects of the Car class has the following properties:
+    /// string LicensePlace
+    /// DateTime Date
+    /// 
+    /// Car objects have two methods: 
+    /// Price() which returns a double of the price of a ticket for that vehicletype.
+    /// VehicleType() which returns a string of the type of vehicle the ticket is for.
+    /// </summary>
+    public class Car
+    {
+        public string LicensePlate { get;}
+        public DateTime Date { get;}
+
+        public Car()
+        {
+
+        }
+        public Car(string licensePlate, DateTime date)
+        {
+            LicensePlate = licensePlate;
+            Date = date;
+        }
+
+        /// <summary>
+        /// The Price method returns a double of the price of a ticket for that vehicletype.
+        /// </summary>
+        /// <returns>double 240</returns>
+        public double Price()
+        {
+            return 240;
+        }
+
+        /// <summary>
+        /// The VehicleType method returns a string of the type of vehicle the ticket is for
+        /// </summary>
+        /// <returns>string "Car"</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+
+
+    }
+}
---
+++ TicketSystemClassLibraryTests/CarTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketSystemClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystemClassLibrary.Tests
+{
+
+    [TestClass()]
+    public class CarTests
+    {
+        private Car car;
+        [TestInitialize]
+        public void BeforeEachTest()
+        {
+            car = new Car();
+        }
+
+        [TestMethod()]
+        public void PriceTest()
+        {
+            //Arrange
+            // Tom grundet TestInitialize
+
+            //Act
+            double expectedPrice = car.Price();
+            //Assert
+            Assert.AreEqual(expectedPrice, 240);
+        }
+
+        [TestMethod()]
+        public void FailPriceIsBelow239Test()
+        {
+            //Arrange
+
+            //Act
+            double expectedPrice = car.Price();
+            //Assert
+            Assert.IsFalse(expectedPrice < 239);
+        }
+
+        [TestMethod()]
+        public void FailPriceIsabove241Test()
+        {
+            //Arrange
+
+            //Act
+            double expectedPrice = car.Price();
+            //Assert
+            Assert.IsFalse(expectedPrice > 241);
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            //Arrange
+
+            //Act
+            string expectedVehicleType = car.VehicleType();
+            //Assert
+            Assert.AreEqual(expectedVehicleType, "Car");
+        }
+        [TestMethod()]
+        public void VehicleTypeIncorrectTest()
+        {
+            //Arrange
+
+            //Act
+            string expectedVehicleType = car.VehicleType();
+            //Assert
+            Assert.AreNotEqual(expectedVehicleType, "MC");
+        }
+    }
+}
